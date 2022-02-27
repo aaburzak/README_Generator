b4c9830acc569e1e7bdc628f368c4383168cb616
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-02-26 20:24:14 -0500

    edited pseudo code into functioning logic, tested generator to update README.md

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# ReadMe_Generator
-Generates a README.md file
+
+#README Generator
+
+## Description
+Utilizes the inquirer dependency to generate professional styled README.md files for your projects.
+
+## License
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)],
+
+
+## Table Of Contents:
+[Installation] (#installation)
+[Usage] (#usage)
+[Contribution] (#contribution)
+[Tests] (#tests)
+[Questions] (#questions)
+
+
+## Installation
+All necessary dependencies can be insalled with the "npm i" command in terminal. Ensure to do this after cloning this repository.
+
+## Usage
+Once you have created a new repository and installed this app, follow the command line prompts and it will generate a professional README.md for your project!
+
+## Contribution
+This project is not currently accepting contributions.
+
+## Tests
+No formal testing for this project.  It was verified through the creation of this README.md
+
+## Questions
+
+For any questions about this project please contact at https://github.comaaburzak
+
+or by Email at aaron.j.burzak@gmail.com
+    
--- index.js
+++ index.js
@@ -1,53 +1,138 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer.prompt ([
+// ![GitHub](https://img.shields.io/github/license/${userName}/${repository})
 
+inquirer.prompt ([
+        {
+            type: "input",
+            name: "userName",
+            message:"Hello, what is your GitHub username?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message:"Please provide your best email for contact.",
+        },
+        // {
+        //     type: "input",
+        //     name: "repository",
+        //     message:"Please provide the GitHub repository for this project.",
+        // },
         {
             type: "input",
             name: "title",
-            message:"What is the title of the project?"
+            message:"What is the title of the project?",
         },
         {
             type: "input",
             name: "description",
-            message:"What is the description of the project?"
+            message:"Please describe the project.",
         },
         {
             type: "input",
-            name: "userStory",
-            message:"What is the User Story?"
+            name: "installation",
+            message:"What is required for installation of the project?",
         },
         {
             type: "input",
-            name: "username",
-            message:"What is your github username?"
+            name: "usage",
+            message:"Please provide instructions and examples for use.",
         },
-
+        {
+            type: "list",
+            name: "license",
+            message:"Please choose an applicable license for the project.",
+            choices:[
+                "N/A",
+                "Academic Free License v3.0",
+                "Apache license 2.0",
+                "Artistic license 2.0",
+                "Boost Software License 1.0",
+                "BSD 2-clause 'Simplified' License",
+                "BSD 3-clause 'New' or 'Revised' license",
+                "BSD 3-clause Clear license",
+                "Creative Commons license family",
+                "Creative Commons Zero v1.0 Universal",
+                "Creative Commons Attribution 4.0",
+                "Creative Commons Attribution Share Alike 4.0",
+                "Do What The F*ck You Want To Public License (yes this is real)",
+                "Educational Community License v2.0",
+                "Eclipse Public License 1.0",
+                "Eclipse Public License 2.0",
+                "European Union Public License 1.1",
+                "GNU Affero General Public License v3.0",
+                "GNU General Public License family",
+                "GNU General Public License v2.0",
+                "GNU General Public License v3.0",
+                "GNU Lesser General Public License v2.1",
+                "GNU Lesser General Public License v3.0",
+                "ISC",
+                "LaTeX Project Public License v1.3c",
+                "Microsoft Public License",
+                "MIT",
+                "Mozilla Public License 2.0",
+                "Open Software License 3.0",
+                "PostgreSQL License",
+                "SIL Open Font License 1.1",
+                "University of Illinois/NCSA Open Source License",
+                "The Unlicense",
+                "zLib License",
+            ]
+        },
         {
             type: "input",
-            name: "repo",
-            message:"please provide repository"
+            name: "contribution",
+            message:"Please provide instructions for those who wish to contribute to the project.",
         },
-
+        {
+            type: "input",
+            name: "tests",
+            message:"Please provide any tests written for the project, and examples on how to run them. ",
+        },
     ])
 
 
-    .then(({title, description, license, user, linkedin, github}) => {
-    fs.writeFile('README.md')
+    .then(({userName, email, title, description, installation, usage, license, contribution, tests}) => {
+    fs.writeFile('README.md',
 
 `
 #${title}
 
 ## Description
- ${description}
+${description}
 
 ## License
-![GitHub](https://img.shields.io/github/license/github-username-here/repo-name-here)
+[![License](https://img.shields.io/badge/License-${license}-blue.svg)],
 
-## User
 
-## Github
+## Table Of Contents:
+[Installation] (#installation)
+[Usage] (#usage)
+[Contribution] (#contribution)
+[Tests] (#tests)
+[Questions] (#questions)
+
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contribution
+${contribution}
+
+## Tests
+${tests}
+
+## Questions
+
+For any questions about this project please contact at https://github.com${userName}
+
+or by Email at ${email}
     
-`
-})
+`,
+        (err) => err ? console.error(err) : console.log('Successfully Generated README.md!'))
+
+});
